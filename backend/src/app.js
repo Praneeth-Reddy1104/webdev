e8fb2862176a7f81d213bc0d3f20e1abc1ced7bc
Praneeth-Reddy1104 <praneethreddynimma2005@gmail.com>
2026-05-25 12:16:17 +0530

    feat: Add CRUD APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,14 @@ const app = express();// create an express app
 app.use(express.json());// middleware to parse JSON request bodies
 //routes import
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+
 
 
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
-
+app.use("/api/v1/posts", postRouter);
 // example route: http://localhost:4000/api/v1/users/register
 
 export default app;
